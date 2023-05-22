470882b2d1a95e7bd3614975656184d23ac0fa37
Burak Albayrak <albayrak1burak@gmail.com>
2023-05-22 17:11:35 +0300

    pyhsics

--- Assets/Script/Game/Top.cs
+++ Assets/Script/Game/Top.cs
@@ -2,14 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class Top : MonoBehaviour
 {
-   
     [SerializeField] private GameManager _GameManager;
     [SerializeField] private AudioSource Topsesi;
-    
-   
+    [SerializeField] private float UygulanacakGuc;
+
+    private Rigidbody topRigidbody;
+    private float dususAcisi;
+
+    private void Start()
+    {
+        topRigidbody = GetComponent<Rigidbody>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         Topsesi.Play();
@@ -17,7 +23,6 @@ public class Top : MonoBehaviour
         if (other.CompareTag("Basket"))
         {
             _GameManager.Basket(transform.position);
-
         }
         else if (other.CompareTag("OyunBitti"))
         {
@@ -28,11 +33,15 @@ public class Top : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         Topsesi.Play();
+
+        if (collision.gameObject.CompareTag("Platform"))
+        {
+            Vector3 platformYonu = collision.contacts[0].normal;
+            Vector3 dususYonu = -collision.relativeVelocity.normalized;
+            dususAcisi = Vector3.SignedAngle(dususYonu, Vector3.down, Vector3.forward);
+
+            Vector3 gucYonu = Quaternion.Euler(0f, dususAcisi * 2f, 0f) * platformYonu;
+            topRigidbody.AddForce(gucYonu * UygulanacakGuc, ForceMode.Force);
+        }
     }
-
-
-    
-
-    
-    
 }
